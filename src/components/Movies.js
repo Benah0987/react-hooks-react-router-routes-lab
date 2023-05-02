8f36a92f3f2b91e5807a4d4168bea18ef9e5f4e0
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-02 15:56:12 +0300

    completed

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { movies } from "../data";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+const Movies = () => {
+  const movieItems = [];
+  for (let i = 0; i < movies.length; i++) {
+    const movie = movies[i];
+    const genreItems = [];
+    for (let j = 0; j < movie.genres.length; j++) {
+      const genre = movie.genres[j];
+      genreItems.push(<li key={genre}>{genre}</li>);
+    }
+    movieItems.push(
+      <div key={movie.title}>
+        <h2>{movie.title}</h2>
+        <p>Runtime: {movie.time} min.</p>
+        <ul>{genreItems}</ul>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieItems}
+    </div>
+  );
+};
 
 export default Movies;
